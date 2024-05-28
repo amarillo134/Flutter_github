90fa8b71abbf43ad7afeef7986b758987fe9ba80
Nezar Shaaban <nezar.shaaban@adesso-mobile.de>
2024-05-28 13:59:00 +0200

    [update-dart-github-action] remove unused imports / code

--- test/lib/features/food/food_list_test.dart
+++ test/lib/features/food/food_list_test.dart
@@ -1,30 +1,25 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_github/features/food/food_list.dart';
 import 'package:flutter_github/features/food/state/food_bloc.dart';
 import 'package:flutter_github/features/food/state/food_event.dart';
 import 'package:flutter_github/features/food/state/food_state.dart';
 import 'package:bloc_test/bloc_test.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
 
 class MockFoodBloc extends MockBloc<FoodEvent, FoodState> implements FoodBloc {}
 
 void main() {
-  late MockFoodBloc mockFoodBloc;
+  // late MockFoodBloc mockFoodBloc;
+  //
+  // setUp(() {
+  //   mockFoodBloc = MockFoodBloc();
+  // });
 
-  setUp(() {
-    mockFoodBloc = MockFoodBloc();
-  });
-
-  Widget widgetsWrapper(Widget body) {
-    return BlocProvider<FoodBloc>(
-      create: (_) => FoodBloc(),
-      child: MaterialApp(
-        home: body,
-      ),
-    );
-  }
+  // Widget widgetsWrapper(Widget body) {
+  //   return BlocProvider<FoodBloc>(
+  //     create: (_) => FoodBloc(),
+  //     child: MaterialApp(
+  //       home: body,
+  //     ),
+  //   );
+  // }
   //
   // testWidgets(
   //   'show circularProgressIndicator on loading',
