99a72056591e2b85eded48a9995648b7acd90ca8
Nezar Shaaban <nezar.shaaban@adesso-mobile.de>
2024-05-28 14:37:10 +0200

    [update-dart-github-action] food_list_test.dart

--- test/lib/features/food/food_list_test.dart
+++ test/lib/features/food/food_list_test.dart
@@ -1,37 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_github/features/food/food_list.dart';
 import 'package:flutter_github/features/food/state/food_bloc.dart';
-import 'package:flutter_github/features/food/state/food_event.dart';
 import 'package:flutter_github/features/food/state/food_state.dart';
-import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
-class MockFoodBloc extends MockBloc<FoodEvent, FoodState> implements FoodBloc {}
+import 'food_list_test.mocks.dart';
 
-//void main() {
-  // late MockFoodBloc mockFoodBloc;
-  //
-  // setUp(() {
-  //   mockFoodBloc = MockFoodBloc();
-  // });
+@GenerateNiceMocks([MockSpec<FoodBloc>()])
+void main() {
+  late MockFoodBloc mockFoodBloc;
 
-  // Widget widgetsWrapper(Widget body) {
-  //   return BlocProvider<FoodBloc>(
-  //     create: (_) => FoodBloc(),
-  //     child: MaterialApp(
-  //       home: body,
-  //     ),
-  //   );
-  // }
-  //
-  // testWidgets(
-  //   'show circularProgressIndicator on loading',
-  //   (widgetTester) async {
-  //     // arrange
-  //     when(mockFoodBloc.state).thenReturn(const LoadingState());
-  //     // act
-  //     await widgetTester.pumpWidget(widgetsWrapper(const FoodList()));
-  //
-  //     // assert
-  //     final indicator = find.byType(CircularProgressIndicator);
-  //     expect(indicator, findsOneWidget);
-  //   },
-  // );
-//}
+  setUp(() {
+    mockFoodBloc = MockFoodBloc();
+  });
+
+  Widget widgetsWrapper(Widget body) {
+    return BlocProvider<FoodBloc>(
+      create: (_) => FoodBloc(),
+      child: MaterialApp(
+        home: body,
+      ),
+    );
+  }
+
+  testWidgets(
+    'show circularProgressIndicator on loading',
+    (widgetTester) async {
+      // arrange
+      when(mockFoodBloc.state).thenReturn(const LoadingState());
+      // act
+      await widgetTester.pumpWidget(widgetsWrapper(const FoodList()));
+
+      // assert
+      final indicator = find.byType(CircularProgressIndicator);
+      expect(indicator, findsOneWidget);
+    },
+  );
+}
